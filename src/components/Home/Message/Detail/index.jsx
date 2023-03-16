b9a0b638082f16537f666c46007c769c49591ef6
Jully Ye <jye@advin.local>
2023-03-16 15:28:29 -0700

    final

--- src/components/Home/Message/Detail/index.jsx
+++ src/components/Home/Message/Detail/index.jsx
@@ -1,21 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { useParams } from 'react-router-dom';
 
-export default function Detail () {
-    const detailData =[
-        { id: '01', content: 'sunny' },
-        { id: '02', content: 'cloudy' },
-        { id: '03', content: 'foggy' }
-    ] 
+const Detail=()=> {
     const params = useParams()
-    const { id, title } = params
-    // const data=detailData.filter(i=>i.id===id)[0]    
+    const { id, title ,content} = params
+     
     return (
     <ul>
             <li>ID:{id}</li>    
             <li>TITLE:{title} </li>  
-            <li>CONTENT:content</li>  
+            <li>CONTENT:{content}</li>  
     </ul>
     )
   }
+export default Detail;
 
--- src/components/Home/Message/index.jsx
+++ src/components/Home/Message/index.jsx
@@ -1,35 +1,42 @@
-import React, { Component } from 'react'
-import { Link, Route,Routes } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, Route,Routes,useNavigate } from 'react-router-dom'
 import Detail from './Detail'
-  
-export default class Message extends Component {
-  state = {
-    messageArr: [
-      { id: '001', title: 'message1' },
-      { id: '002', title: 'message2' },
-      { id: '003', title: 'message3' }
-    ]
-  }
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.state.messageArr.map((mesObj) => {
-            return (
-              <li key={mesObj.id}>
-                {/* 向路由组件传递params参数 */}
-                <Link to={`detail/${mesObj.id}/${mesObj.title}`}>{mesObj.title}</Link>
-              </li>
-            )
-           })}
-        </ul>
-        <hr></hr>
-        {/*声明接收params参数*/}
-        <Routes>
-        <Route path='detail/:id/:title' element={<Detail/>} />
-        </Routes>
-        
-      </div>
-    )
-  }
+
+export default function Message() {
+  const navigate=useNavigate()//只能用在function中
+   const [messageArr]= useState([
+      { id: '001', title: 'message1',content:'sunny' },
+      { id: '002', title: 'message2',content:'rainy' },
+      { id: '003', title: 'message3',content:'cloudy' }
+  ])
+  return (
+    <div>
+      <ul>
+        {messageArr.map((mesObj) => {
+          return (
+            <li key={mesObj.id}>
+              {/* 向路由组件传递params参数 */}
+              <Link to={`detail/${mesObj.id}/${mesObj.title}/${mesObj.content}`}>{mesObj.title}</Link>
+              {/*编程式路由导航：按后退按钮会留下查看痕迹，有历史记录*/}
+              <button onClick={() => navigate(`detail/${mesObj.id}/${mesObj.title}`)}>push</button>
+              {/*编程式路由导航： 按后退按钮不会留下查看痕迹，没有历史记录*/}
+              <button onClick={() => navigate(`detail/${mesObj.id}/${mesObj.title}`, { replace: true })}>replace</button>
+            </li>
+          )
+        })
+        }
+      </ul>
+      <hr></hr>
+      {/*声明接收params参数*/}
+      <Routes>
+        <Route path='detail/:id/:title/:content' element={<Detail />} />
+      </Routes>
+      {/*按back后退一步*/}
+      <button onClick={() => { navigate(-1) }}>back</button>
+      {/*按forward前进一步*/}
+      <button onClick={() => { navigate(1) }}> forward</button>
+      {/*按go前进两步*/}
+      <button onClick={()=>{navigate(2)}}>go</button>
+    </div>
+  )
 }
